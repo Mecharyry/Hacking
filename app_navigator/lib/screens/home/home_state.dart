06f0daeb9c6c6137e791f4e97a03a490697c0b61
Ryan Feline <ryan.feline@trint.com>
2022-05-28 12:16:56 +0100

    Remove unnecessary states from home state.

--- app_navigator/lib/screens/home/home_state.dart
+++ app_navigator/lib/screens/home/home_state.dart
@@ -7,8 +7,4 @@ part 'home_state.freezed.dart';
 class HomeState with _$HomeState {
   factory HomeState.loading() = _Loading;
   factory HomeState.loaded() = _Loaded;
-
-  factory HomeState.showBanner({required final String snackbarMessage}) = _ShowSnackbar;
-  factory HomeState.showDialog({required final String dialogMessage}) = _ShowDialog;
-  factory HomeState.showBottomSheet({required final String bottomSheetMessage}) = _ShowBottomSheet;
 }
